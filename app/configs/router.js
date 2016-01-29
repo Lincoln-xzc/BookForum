12d832a6f7c7be6a40e08cb5efc3e6dfbe3d6032
xiezc <xzc5424813657@sina.com>
2016-01-29 17:38:31 +0800

    first commit

---
+++ app/configs/router.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('app').config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('default', {
+    url: '',
+    templateUrl: 'controllers/home/index.html',
+    controller: 'HomeIndexCtrl'
+  });
+  //副路由
+  $urlRouterProvider.state('reader',{
+    url:'/reader',
+    //父路由一般只要提供一个这样的template就够了，不必要使用templateUrl,页面中公共的部分通过组件型指令去实现会更灵活、漂亮
+    template:'<div ui-view></div>',
+    //抽象路由不能通过URL直接访问，比如直接访问/reader路径会跳转到otherwise中去
+    abstract:true
+  });
+  //定义一个子路由
+  $urlRouterProvider.state('reader.create',{
+    url:'/create',
+    templateUrl:'controllers/reader/create.html',
+    controller:'ReaderCreateCtrl as vm'
+  });
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'controllers/home/index.html',
+    controller: 'HomeIndexCtrl'
+  });
+
+  $stateProvider.state('notFound', {
+    url: '/notFound',
+    templateUrl: 'controllers/home/notFound.html',
+    controller: 'HomeNotFoundCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/notFound');
+});
---
+++ app/controllers/reader/create.js
@@ -0,0 +1,14 @@
+'use strict'
+/**
+ * Created by Administrator on 2016/1/29.
+ */
+//引用应用模板
+angular.module('app')
+.controller('ReaderCreateCtrl',function ReaderCreateCtrl(){
+    //在ReaderCreateCtrl as vm语法下，当前函数的this指针指向的其实是$scope.vm变量，作为一项约定和最佳实践，我们把他赋值给vm变量。我们在程序中不在直接使用this,因为js中的this
+    //很容易给一些不熟悉js的程序员造成混乱
+     var vm = this;
+     vm.submit = function(form){
+       console.log(form);
+     };
+})
